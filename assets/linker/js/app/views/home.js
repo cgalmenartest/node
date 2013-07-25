a5a0b8973cb4c5d8ca073821fbb7f588366d1dc7
Dain Miller <miller.dain@gmail.com>
2013-07-25 15:20:44 +0000

    Backbone push, attempting to load files to test.

---
+++ assets/linker/js/app/views/home.js
@@ -0,0 +1,14 @@
+var HomeView = Backbone.View.extend({
+
+    el: $("#container"),
+
+    initialize: function () {
+    	console.log("Firing up home view")
+        // this.render();
+    },
+
+    // render: function () {
+    //     this.$el.html(this.template).hide().fadeIn();
+    // }
+
+});    
