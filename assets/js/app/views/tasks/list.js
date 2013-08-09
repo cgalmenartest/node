89fa204f624fa3f4796de2d748d3a98a8a420878
Dain Miller <miller.dain@gmail.com>
2013-08-09 11:10:18 -0400

    Code style updates, and various variable name refactorings.

--- assets/js/app/views/tasks/list.js
+++ assets/js/app/views/tasks/list.js
@@ -24,21 +24,20 @@ define([
       var template = _.template(TaskListTemplate, data);
       this.$el.html(template).hide().fadeIn();
       new TaskFormView().render()
-
+      
       app.events.on("task:render", function () {
         // Hard wipe out the modal upon the rendering of a task (success)
         $(".modal a[href='#addTask']").modal('hide');
         $('body').removeClass('modal-open');
         $('.modal-backdrop').remove();
 
-        var height = $("#task-list-wrapper").height();
-        
-        var len = $("#task-list-wrapper").children().find('li').length 
-        var scrollLength = len * 90;
+        var heightOfSingleTask  = 90,
+            taskCount           = $("#task-list-wrapper").children().find('li').length,
+            scrollLength        = taskCount * heightOfSingleTask;
 
+        // Set at a 5ms timeout before the scroll for user experience purposes
         setTimeout(function () {
-          $("#task-list-wrapper").animate({ scrollTop: scrollLength })
-          console.log($("#task-list-page"))
+          $("#task-list-wrapper").animate({ scrollTop: scrollLength });
         }, 500);
       });
 
