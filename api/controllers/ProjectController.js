2c658256f7ad7c481449abe011884cff9f45f435
Joe Polastre <joe@polastre.com>
2013-08-12 14:37:03 +0000

    Associate projects with owners
    When calling /find on a project, return the owners of the project
    in the `owners` field.
    
    Allow an owner to add another owner via a POST to /projectowner.
    
    Allow an owner to delete an owner via DELETE to /projectowner or
    /projectowner/destroy.  Be sure to use the `id` of the index in the
    projectowner table (found in project.owners.id when you call find)
    in order to perform the delete operation.

--- api/controllers/ProjectController.js
+++ api/controllers/ProjectController.js
@@ -7,6 +7,21 @@
 
 module.exports = {
 
+  find: function(req, res) {
+    // Find a given project and return the full information including owners.
+    Project.findOneById(req.route.params.id, function (err, proj) {
+      if (err) { return res.send(400, { message: 'Error finding project.' } ); }
+      if (!proj) { return res.send({}); }
+      ProjectOwner.findByProjectId(proj.id, function(err, owners) {
+        proj.owners = [];
+        for (var i = 0; i < owners.length; i++) {
+          proj.owners.push({ id: owners[i].id, userId: owners[i].userId });
+        }
+        res.send(proj);
+      });
+    });
+  },
+
 	// Namespace the find() method that returns the array of objects into
 	// a familiar findAll method. 
 	findAll: function (req, res) {
@@ -14,6 +29,22 @@ module.exports = {
 			if (err) return res.send(err, 500);
 			res.send({ projects: projects });
 		});
-	}
+	},
+
+  create: function (req, res) {
+    if (req.route.method != 'post') { return res.send(400, { message: 'Unsupported operation.' } ); }
+    var proj = _.extend(req.body || {}, req.params);
+    Project.create(proj, function (err, newProj) {
+      if (err) { return res.send(400, { message: 'Error creating project.' } ); }
+      // Associate the user that created this project with the project
+      ProjectOwner.create({ projectId: newProj.id,
+                            userId: req.user[0].id
+                          }, function (err, projOwner) {
+        if (err) { return res.send(400, { message: 'Error storing project owner.' } ); }
+        newProj.owners = [ projOwner ];
+        return res.send(newProj);
+      });
+    });
+  }
 
 };
