f4afb7aa74e4bbdc98523277285fe38eb67aa497
Dain Miller <miller.dain@gmail.com>
2013-09-04 11:38:14 -0400

    Syntax update.

--- assets/js/backbone/mixins/utilities.js
+++ assets/js/backbone/mixins/utilities.js
@@ -22,7 +22,7 @@ var getCurrentProjectModelFromFormAttributes = function (collection, attr) {
 			for ( ; i < models.length; i += 1) {
 
 				// Loop through all the attributes within the model
-				for (var j in models[i].attributes) {
+				for (j in models[i].attributes) {
 
 					// If we find the model.attr[value] on the current models
 					// attribute that we are looping over, then stop and return that
