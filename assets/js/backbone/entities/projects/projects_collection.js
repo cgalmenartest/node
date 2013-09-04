6d446cf8d2a18bcf05c97814190fb4242c6849c8
Dain Miller <miller.dain@gmail.com>
2013-09-04 11:24:18 -0400

    Removed uneeded comments on Collection.

--- assets/js/backbone/entities/projects/projects_collection.js
+++ assets/js/backbone/entities/projects/projects_collection.js
@@ -8,8 +8,6 @@ define([
   
     model: ProjectModel,
 
-    project: null,
-
     url: '/project/findAll',
 
     parse: function (response) {
@@ -34,20 +32,3 @@ define([
 
   return ProjectsCollection;
 });
-
-// Archived to refer back to:
-//   this.on("project:post", function (title, description) {
-//     var project = new ProjectModel({ title: title, description: description })
-//     _this.add(project);
-//     _this.models.forEach(function (_model) {
-//       _model.save();
-//     });
-//     app.events.trigger("projectSave:success");
-//   });
-
-//   app.events.on("projectSave:success", function () {
-//     _this.fetch();
-//     app.events.trigger("project:render");
-//   });
-
-// },
