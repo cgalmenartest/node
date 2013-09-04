da15edd355776350390041af6c5127934d5b0c53
Joe Polastre <joe@polastre.com>
2013-09-04 00:04:04 +0000

    Migrate controllers to Sails 0.9
    These controllers were still using the Sails 0.8
    blueprint.  No actual code change, just different
    code framework.

--- api/controllers/TagController.js
+++ api/controllers/TagController.js
@@ -1,10 +1,16 @@
-/*---------------------
-	:: Tag 
-	-> controller
----------------------*/
-var TagController = {
+/**
+ * TagController
+ *
+ * @module    :: Controller
+ * @description :: Contains logic for handling requests.
+ */
 
+module.exports = {
 
+  /* e.g.
+  sayHello: function (req, res) {
+    res.send('hello world!');
+  }
+  */
 
 };
-module.exports = TagController;
