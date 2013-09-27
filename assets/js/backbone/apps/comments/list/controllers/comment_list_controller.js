736f48bdc14a41f406a599de0ad4d250b2db6e5f
Joe Polastre <joe@polastre.com>
2013-09-27 09:01:35 +0000

    Connect UI to comment topic feature
    Make changes to the UI, UI views, and UI controllers to
    support the notion of discussion topics.
    
    The "new topic" button drops down an input box for the
    topic content.  This is then saved via an AJAX call and
    the collection is refreshed.  DM: please fix this to
    use the appropriate collection calls if it shouldn't work
    this way, which is outside the backbone framework.
    
    The comment-list-wrapper now renders all of the comment
    UI, including the header.  Inside of each comment-list,
    when there's a topic, it renders the topic and then
    the comments for that topic, then a placeholder for
    the comment form at the end.
    
    After the view is rendered, the new comment sub view
    is rendered for each of the topics and inserted into
    its comment-form-wrapper.  The new comment view has
    been modified to be passed the topicId (parentId)
    as an option, so it can relay that to the backend.

--- assets/js/backbone/apps/comments/list/controllers/comment_list_controller.js
+++ assets/js/backbone/apps/comments/list/controllers/comment_list_controller.js
@@ -20,24 +20,55 @@ define([
 
     initialize: function () {
       this.initializeCommentCollection();
+      this.initializeListeners();
       this.commentCollection.on("comment:save:success", this.initializeCommentCollection())
     },
 
     initializeCommentCollection: function () {
       var self = this;
+      this.commentCollection = new CommentCollection();
 
-        this.commentCollection = new CommentCollection();
+      this.commentCollection.fetch({
+        url: '/comment/findAllByProjectId/' + this.options.projectId,
+        success: function (collection) {
+          self.renderView(collection);
+        }
+      });
+    },
 
-        this.commentCollection.fetch({
-          url: '/comment/findAllByProjectId/' + this.options.projectId,
-          success: function (collection) {
-            self.renderView(collection);
-          }
-        })
+    initializeListeners: function() {
+      var self = this;
 
+      this.listenTo(this.commentCollection, "comment:topic:new", function (value) {
+        var data = {
+          projectId: self.options.projectId,
+          value: value,
+          topic: true
+        };
+        // TODO: DM: Fix this to add to the collection appropriately,
+        // and fetch/re-render as needed.  This is a hack to get it to work
+        $.ajax({
+          url: '/comment',
+          type: 'POST',
+          contentType: 'application/json',
+          processData: false,
+          data: JSON.stringify(data)
+        }).done(function (result) {
+          self.commentCollection.fetch({
+            url: '/comment/findAllByProjectId/' + self.options.projectId,
+            success: function (collection) {
+              self.commentListView = new CommentListView({
+                el: ".comment-list-wrapper",
+                collection: collection
+              }).render();
+            }
+          });
+        });
+      });
     },
 
     renderView: function (collection) {
+      var self = this;
       if (this.commentListView) {
         this.commentListView.cleanup();
       } 
@@ -51,8 +82,17 @@ define([
       var self = this;
       rendering.trigger("renderForm")
       rendering.once("renderForm", function () {
-        if (this.commentForm) this.commentForm.cleanup();
-        this.commentForm = new CommentFormView({ projectId: self.options.projectId, collection: collection });  
+        var comments = self.commentCollection.toJSON()[0].comments;
+        for (var i = 0; i < comments.length; i++) {
+          if (comments[i].topic === true) {
+            var view = new CommentFormView({
+              el: '#comment-topic-' + comments[i].id,
+              projectId: self.options.projectId,
+              parentId: comments[i].id,
+              collection: collection
+            });
+          }
+        }
       })
       
     },
