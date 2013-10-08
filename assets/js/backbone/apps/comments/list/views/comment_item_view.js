0d34ed95603da942497359bbadbd91562fde0ca5
Joe Polastre <joe@polastre.com>
2013-10-08 14:01:34 +0000

    Quick fix for part 8, issue #17
    Wanted to demo this functionality so put in a quick
    fix for showing links.  This will only work
    for comments and not for topics.
    
    The whole thing will have to be overhauled anyway when Dain
    implements the spec for the inline autocomplete.

--- assets/js/backbone/apps/comments/list/views/comment_item_view.js
+++ assets/js/backbone/apps/comments/list/views/comment_item_view.js
@@ -11,10 +11,18 @@ define([
 
     render: function () {
       // Clean string out from undefineds in the marshalling process.
-      cleanStringFromUndefined(this.model, this.model.value, "||")
+      cleanStringFromUndefined(this.model, this.model.value, "||");
 
-      var data = { comment: this.model },
-          compiledTemplate = _.template(CommentItemTemplate, data);
+      var data = { comment: this.model };
+      console.log(this.model.value.indexOf('||'));
+
+      if (this.model.value.indexOf('||') != -1) {
+        data['comment']['link'] = this.model.value.split('||')[1];
+        data['comment']['value'] = this.model.value.split('||')[0];
+      }
+      console.log(data);
+
+      compiledTemplate = _.template(CommentItemTemplate, data);
 
       this.$el.append(compiledTemplate);
       $("time.timeago").timeago();
