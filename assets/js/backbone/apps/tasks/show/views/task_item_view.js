016c7515d58cf408cebbe38c04110b397be99b1c
Dain Miller <miller.dain@gmail.com>
2013-11-01 17:00:53 -0400

    Updated Tasks Show View Logic
    Updated the logic and initialization on the task show view to behave the
    same as Profiles (to an extent).  There is now the ability to render the
    view state (tags, task specific data, etc) and then edit that as well,
    fetching all the data needed right there in the show view.  Eliminating
    the need to do it again in the edit view.  Template files have yet to be
    checked in, still working out bugs.

--- assets/js/backbone/apps/tasks/show/views/task_item_view.js
+++ assets/js/backbone/apps/tasks/show/views/task_item_view.js
@@ -14,8 +14,114 @@ define([
     el: "#container",
 
     render: function () {
+      this.initializeSelect2Data();
+
       var compiledTemplate = _.template(TaskShowTemplate, this.model.toJSON());
       $(this.el).html(compiledTemplate)
+
+      var tags = [
+        $("#topics").select2('data'),
+        $("#skills").select2('data'),
+        $("#skills-required").select2('data'),
+        $("#people").select2('data'),
+        $("#time-required").select2('data'),
+        $("#length").select2('data'),
+        // $("#time-estimate").select2('data'),
+        // $("#task-location").select2('data'),
+        $("#input-specific-location").val(),
+      ];
+      this.initTaskTags(tags);
+    },
+
+    initializeSelect2Data: function () {
+      var self = this,
+          types = ["skills-required", "time-required", "people", "length", "time-estimates"];
+
+      this.tagSources = {};
+
+      var requestAllTagsByType = function (type) {
+        $.ajax({
+          url: '/api/ac/tag?type=' + type + '&list',
+          type: 'GET',
+          async: false,
+          success: function (data) {
+            // Dynamically take the hyphen delimited type (if more than 1 word) and
+            // camelize it like the template expects.  Then create an associative
+            // array based on that for the pointer to the list itself to be iterated through
+            // on the front-end.
+            var typeArray = type.split("-");
+            if (typeArray.length > 1) {
+              typeArray[typeArray.length - 1] = typeArray[typeArray.length - 1].charAt(0).toUpperCase() + typeArray[typeArray.length - 1].substr(1).toLowerCase();
+              self.tagSources[typeArray.join("")] = data;
+            } else {
+              self.tagSources[type] = data;
+            }
+          }
+        });
+      }
+
+      async.each(types, requestAllTagsByType, function (err) {
+        self.render();
+      });
+    },
+
+    initializeTags: function () {
+      this.tagsView = new TagShowView({
+        model: this.model,
+        el: '.tag-wrapper',
+        target: 'task',
+        url: '/api/tag/findAllByTaskId'
+      });
+    },
+
+    initTaskTags: function (tags) {
+      var self = this,
+          tagMap;
+
+      var removeTag = function (type, done) {
+        if (self.model[type]) {
+          if (self.model[type].tagId) {
+            return done();
+          }
+        $.ajax({
+          url: '/api/tag/' + self.model[type].tagId,
+          type: 'DELETE',
+          success: function (data) {
+            return done();
+          }
+        });
+        return;
+        }
+        return done
+      };
+
+      var addTag = function (tag, done) {
+        if (!tag || !tag.id) {
+          return done();
+        }
+
+        tagMap = {
+          tagId: tag.id,
+          taskId: this.model.id
+        }
+
+        $.ajax({
+          url: '/api/tag',
+          type: 'POST',
+          data: tagMap
+        }).done(function (data) {
+          done();
+        });
+      }
+
+      async.each(tags, addTag, function (err) {
+        return self.model.trigger("task:tags:save:success", err);
+      });
+
+      this.listenTo(self.model, "task:tags:save:success", function (data) {
+        Backbone.history.navigate('taskShow', { trigger: true })
+      });
+
     },
 
     cleanup: function () {
