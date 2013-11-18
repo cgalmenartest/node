02b99d148d0685403c12f9a2de215af0879d3f49
Dain Miller <miller.dain@gmail.com>
2013-11-18 00:02:45 -0500

    Checkin of my implementation for task item view.

--- assets/js/backbone/apps/tasks/show/views/task_item_view.js
+++ assets/js/backbone/apps/tasks/show/views/task_item_view.js
@@ -5,8 +5,9 @@ define([
   'backbone',
   'async',
   'base_view',
+  'tag_config',
   'text!task_show_template'
-], function (Bootstrap, Popovers, _, Backbone, async, BaseView, TaskShowTemplate) {
+], function (Bootstrap, Popovers, _, Backbone, async, BaseView, TagConfig, TaskShowTemplate) {
 
   var TaskItemView = BaseView.extend({
 
@@ -35,13 +36,37 @@ define([
           var data = {};
           self.model.attributes['tags'] = tagData;
 
-          _.each(self.model.toJSON().tags, function (tag) {
-            _.each(TagConfig.tags, function (config) {
-              if (tag.tag.type === config.type) {
-                data[config.type] = tag.tag
+          var tags = [];
+          var validTags = {};
+
+          // Attempting a push into a fresh array to fix a bug I'm seeing
+          _.each(self.model.toJSON().tags(), function (tag) {
+            tags.push(tag.tag);
+          }
+
+          // Attempting to build up an obj that stores the tags
+          // and the config data so that I can have access to both
+          // on the front-end layer after validating the server did indeed
+          // return the propper tag types for this entity (task).
+          for (var i in TagConfig.tags) {
+            for (var j = 0; j < tags.length; j += 1) {
+              if (TagConfig.tags[i].type === tags[j].type) {
+                validTags[tags[j].type + "_config"] = TagConfig.tags[i];
+                validTags[TagConfig.tags[i].type] = tags[j];
               }
-            })
-          });
+            }
+          }
+
+          // I can't depend on a key to be the same, so push it into an array
+          var keys = _.keys(validTags)
+
+          // At this point we can confirm it is a valid type
+          // Add the type to the data object to be retrieved in the view
+          for (var k in vaidTags) {
+            for (var l in keys) {
+              data[keys[l]] = validTags[k];
+            }
+          }
 
           data['model'] = self.model.toJSON();
 
@@ -66,7 +91,7 @@ define([
 
     initializeSelect2Data: function () {
       var self = this,
-          types = ["skills-required", "time-required", "people", "length", "time-estimates"];
+          types = ["skillsRequired", "timeRequired", "people", "length", "timeEstimates"];
 
       this.tagSources = {};
 
@@ -76,19 +101,11 @@ define([
           type: 'GET',
           async: false,
           success: function (data) {
-
-            console.log(data);
             // Dynamically take the hyphen delimited type (if more than 1 word) and
             // camelize it like the template expects.  Then create an associative
             // array based on that for the pointer to the list itself to be iterated through
             // on the front-end.
-            var typeArray = type.split("-");
-            if (typeArray.length > 1) {
-              typeArray[typeArray.length - 1] = typeArray[typeArray.length - 1].charAt(0).toUpperCase() + typeArray[typeArray.length - 1].substr(1).toLowerCase();
-              self.tagSources[typeArray.join("")] = data;
-            } else {
               self.tagSources[type] = data;
-            }
           }
         });
       }
