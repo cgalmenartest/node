f5bb910b79b617a5cb6e74f11b2f359872d931fb
Joe Polastre <joe@polastre.com>
2014-01-08 21:09:53 +0000

    Use IN query by passing in array.
    when finding the audience, if you look up a particular
    column with an array instead of a value, it will execute
    an IN sql query, such as IN ['1','2'...]
    
    This fixes a bug where notifications were being sent to
    everyone in the database.

--- api/services/notifications/audience.js
+++ api/services/notifications/audience.js
@@ -12,14 +12,12 @@
 // convenience function that accepts a collection of entities with a property "userId" (must be exact) and returns a collection of those users
 function convertToUsers (err, userIdPropertyCollection, cb) {
   var uIds = [];
-  _.each(userIdPropertyCollection, function(item){
-    uIds.push({ id: item.userId });
+  _.each(userIdPropertyCollection, function (item) {
+    uIds.push(item.userId);
   });
-  if(uIds.length > 0){
+  if (uIds.length > 0) {
     User.find({
-      where: {
-        or: uIds
-      }
+      where: { id: uIds }
     }).done(cb);
   }
   else {
@@ -29,28 +27,28 @@ function convertToUsers (err, userIdPropertyCollection, cb) {
 
 module.exports = {
 
-  findAllUsers: function(fields, settings, cb){
+  findAllUsers: function (fields, settings, cb) {
     User.find({}).done(cb);
   },
-  findUser: function(fields, settings, cb){
+  findUser: function (fields, settings, cb) {
     User.find({id: fields.userId}).done(cb);
   },
-  findProjectOwners: function(fields, settings, cb){
+  findProjectOwners: function (fields, settings, cb) {
     ProjectOwner.find({projectId: fields.projectId}).done(function(err, owners){
       convertToUsers(err, owners, cb);
     });
   },
-  findProjectParticipants: function(fields, settings, cb){
+  findProjectParticipants: function (fields, settings, cb) {
     // todo
   },
-  findProjectLikers: function(fields, settings, cb){
+  findProjectLikers: function (fields, settings, cb) {
     // todo
   },
-  findProjectThreadCommenters: function(fields, settings, cb){
+  findProjectThreadCommenters: function (fields, settings, cb) {
     // todo
   },
-  findProjectThreadParentCommenters: function(fields, settings, cb){
-    Comment.find({id : fields.commentId }).done(function(err, comments){
+  findProjectThreadParentCommenters: function (fields, settings, cb) {
+    Comment.find({id : fields.commentId }).done(function (err, comments) {
       if(!err && comments.length > 0){
         var comment = comments.pop();
         // get all parent comments
@@ -68,21 +66,21 @@ module.exports = {
       }
     });
   },
-  findTaskOwners: function(fields, settings, cb){
+  findTaskOwners: function (fields, settings, cb) {
     Task.find({id: fields.taskId}).done(function(err, owners){
       convertToUsers(err, owners, cb);
     });
   },
-  findTaskParticipants: function(fields, settings, cb){
+  findTaskParticipants: function (fields, settings, cb) {
     // todo
   },
-  findTaskLikers: function(fields, settings, cb){
+  findTaskLikers: function (fields, settings, cb) {
     // todo
   },
-  findTaskThreadCommenters: function(fields, settings, cb){
+  findTaskThreadCommenters: function (fields, settings, cb) {
     // todo
   },
-  findTaskThreadParentCommenters: function(fields, settings, cb){
+  findTaskThreadParentCommenters: function (fields, settings, cb) {
     Comment.find({id : fields.commentId }).done(function(err, comments){
       if(!err && comments.length > 0){
         var comment = comments.pop();
@@ -102,4 +100,4 @@ module.exports = {
     });
   }
 
-}
+};
