8a2da6399808e00bb3093ed5253e6fd3019a3ba8
Joe Polastre <joe@polastre.com>
2014-03-25 17:49:49 +0000

    Fix modal whitespace and comment syntax
    Convert tabs to spaces; use javadoc style comments.

--- assets/js/backbone/components/modal.js
+++ assets/js/backbone/components/modal.js
@@ -1,22 +1,23 @@
-// The Modal component needs to be set to the el: of the parent view
-// you are trying to init it within, then you will be appending the modal-template to that
-// view (this).  Then the form view has an el of $(".modal-body") so that the form will render
-// within that body area.
-// Then all you have to do is set the ID of the modal to the ID of the link you are trying
-// to trigger from as per bootstrap BP.
-
+/**
+ * The Modal component needs to be set to the el: of the parent view
+ * you are trying to init it within, then you will be appending the modal-template to that
+ * view (this).  Then the form view has an el of $(".modal-body") so that the form will render
+ * within that body area.
+ * Then all you have to do is set the ID of the modal to the ID of the link you are trying
+ * to trigger from as per bootstrap BP.
+ */
 define([
   'jquery',
-	'underscore',
-	'backbone',
+  'underscore',
+  'backbone',
   'utilities',
-	'base_component',
+  'base_component',
   'text!modal_template'
 ], function ($, _, Backbone, utils, BaseComponent, ModalTemplate) {
 
-	Application.Component.Modal = BaseComponent.extend({
+  Application.Component.Modal = BaseComponent.extend({
 
-		initialize: function (options) {
+    initialize: function (options) {
       this.options = options;
     },
 
@@ -36,7 +37,7 @@ define([
       removeView(this);
     }
 
-	});
+  });
 
-	return Application.Component.Modal;
-})
+  return Application.Component.Modal;
+});
