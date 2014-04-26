c4becf299cb03b1208219e5855b51a74b8dd49cd
Joe Polastre <joe@polastre.com>
2014-04-26 01:31:23 +0000

    Set HTML title to systemName from local.js
    Use the systemName to set the title of the HTML doc.
    Useful for branding.

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -9,6 +9,7 @@ module.exports = {
   index: function(req, res) {
     // include data variables for the view passed by the policies
     var data = {
+      systemName: sails.config.systemName,
       alert: req.alert || null
     };
     res.view(data);
