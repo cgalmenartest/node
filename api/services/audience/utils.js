916cdee05c46c55b25c5d0e751af818ce71f1dac
Joe Polastre <joe@polastre.com>
2014-04-27 02:14:35 +0000

    Clean util function
    Add documentation, pass err correctly to the callback,
    and make it part of the module instead of a standalone
    function.

--- api/services/audience/utils.js
+++ api/services/audience/utils.js
@@ -6,22 +6,29 @@
  */
 var _ = require('underscore');
 
-// convenience function that accepts a collection of entities with a property "userId" (must be exact) and returns a collection of those users
-function convertToUsers (err, userIdPropertyCollection, cb) {
-  var uIds = [];
-  _.each(userIdPropertyCollection, function (item) {
-    uIds.push(item.userId);
-  });
-  if (uIds.length > 0) {
-    User.find({
-      where: { id: uIds }
-    }).done(cb);
-  }
-  else {
-    cb(null, []);
-  }
-};
-
 module.exports = {
-  convertToUsers: convertToUsers
+  /**
+   * Convenience function that accepts a collection of entities
+   * with a property "userId" (must be exact) and returns a collection of those users
+   * @param err any upstream error that occurred (immediately calls cb(err))
+   * @param userIdPropertyCollection models that have a .userId attribute
+   * @param cb callback function with format (err, userArray)
+   */
+  convertToUsers: function (err, userIdPropertyCollection, cb) {
+    var uIds = [];
+    if (err) {
+      return cb(err, []);
+    }
+    _.each(userIdPropertyCollection, function (item) {
+      uIds.push(item.userId);
+    });
+    if (uIds.length > 0) {
+      User.find({
+        where: { id: uIds }
+      }).done(cb);
+    }
+    else {
+      cb(null, []);
+    }
+  }
 };
