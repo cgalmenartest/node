dfb99099acc4e289c09e9e15c6663156ecd89c02
Sarah Allen <sarah.allen@gsa.gov>
2014-07-25 15:08:04 -0700

    draft projects cannot be viewed when you are not logged in

--- test/api/sails/project.test.js
+++ test/api/sails/project.test.js
@@ -8,6 +8,16 @@ describe('project:', function() {
 
   var publicProject, draftProject;
 
+  // check that the values returned are correct for a project 
+  function assertProject(testData, realData) {
+    assert.equal(testData.title, realData.title);
+    assert.equal(testData.description, realData.description);
+    assert.equal(testData.userId, realData.userId);
+    assert.equal(testData.id, realData.id);
+    assert.equal(testData.state, realData.state);
+  }
+
+
   before(function(done) {
     request = utils.init();
     utils.login(request, function(err) {
@@ -40,18 +50,14 @@ describe('project:', function() {
         assert.equal(response.statusCode, 200);
         var b = JSON.parse(body);
         // check that the values returned are correct 
-        assert.equal(b.title, draftProject.title);
-        assert.equal(b.description, draftProject.description);
-        assert.equal(b.userId, draftProject.userId);
-        assert.equal(b.id, draftProject.id);
-        assert.equal(b.state, "draft");
+        assertProject(b, draftProject)
         assert.equal(b.like, false);
-        done();
       });
-    });
+      done();
 
+    });
   });
-/**
+  
   describe('logged out:', function () {
 
     before(function () {
@@ -59,36 +65,12 @@ describe('project:', function() {
     });
 
     it('view draft project', function (done) {
-      request.get({ url: conf.url + '/Project/' + confirmedProject.id },
-                  function(err, response, body) {
+      request.get({ url: conf.url + '/project/' + draftProject.id },
+                  function (err, response, body) {
         if (err) { return done(err); }
-        var b = JSON.parse(body);
-        // check that the values passed in are the same as those passed back
-        assert.equal(confirmedProject.title, b.title);
-        assert.equal(confirmedProject.description, b.description);
-        assert.equal(confirmedProject.location, b.location);
-        assert.equal(confirmedProject.projectId, b.projectId);
-        // make sure the automatically populated fields get set
-        assert(b.id);
-        assert(b.userId);
-        assert(b.uuid);
-        assert(b.status);
-        // check if rsvp list is populated
-        assert.equal(b.rsvps.length, 1);
-        assert.equal(b.rsvp, false);
-        done();
-      });
-    });
-
-    it('view denied', function(done) {
-      request.get({ url: conf.url + '/Project/' + draftProject.id },
-                  function(err, response, body) {
-        // access should be denied
         assert.equal(response.statusCode, 403);
-        done(err);
       });
+      done();
     });
-
   });
-**/
 });
