cgalmenartest/node

commit cd5f4e298446233e00b7044be81fabd77f81ee4a

Add person's name to alt text
Query the owner's name, include it in the API
response, and then display it in the image ALT
text.  This is needed for 508 compliance #127

api/services/utils/project.js
@@ -1,3 +1,5 @@
+var async = require('async');
+
 /**
  * Determine if a user has access to project
  * Callback: function(err, proj)
@@ -31,16 +33,28 @@ var authorized = function (id, userId, cb) {
 
 var getMetadata = function(proj, user, cb) {
   proj.like = false;
+
+  // look up the name of an owner and include it in the objection
+  var getOwnerName = function (ownerObj, done) {
+    User.findOneById(ownerObj.userId, function (err, owner) {
+      if (err) { return done(err); }
+      ownerObj.name = owner.name;
+      return done();
+    });
+  };
+
   Like.countByProjectId( proj.id, function (err, likes) {
     if (err) { return cb(err, proj); }
     proj.likeCount = likes;
-    if (!user) {
-      return cb(null, proj);
-    }
-    Like.findOne({ where: { userId: user.id, projectId: proj.id }}, function (err, like) {
+    async.each(proj.owners, getOwnerName, function (err) {
       if (err) { return cb(err, proj); }
-      if (like) { proj.like = true; }
-      return cb(null, proj);
+      if (!user) {
+        return cb(null, proj);
+      }
+      Like.findOne({ where: { userId: user.id, projectId: proj.id }}, function (err, like) {
+        if (err) { return cb(err, proj); }
+        if (like) { proj.like = true; }
+      });
     });
   });
 };

assets/js/backbone/apps/projectowner/show/templates/projectowner_show_template.html
@@ -18,7 +18,7 @@
   <% if (data.owners) { %>
     <% for (var i = 0; i < data.owners.length; i++) { %>
     <div class="project-people-div" data-userid="<%= data.owners[i].userId %>">
-      <img src="/api/user/photo/<%= data.owners[i].userId %>" class="project-people"/>
+      <img src="/api/user/photo/<%= data.owners[i].userId %>" class="project-people" alt="<%= data.owners[i].name %>"/>
       <% if ((data.isOwner || user.isAdmin) && (data.owners[i].userId != cache.currentUser.id) && data.edit) { %>
       <a href="#" class="delete-projectowner project-owner-delete glyphicon glyphicon-remove"  id="delete-projectowner-<%= data.owners[i].id %>" data-uid="<%= data.owners[i].userId %>" data-poid="<%= data.owners[i].id %>">
       </a>

commit 68917598dfcc81ddd19544c9f65e47cbd87b7790

make it so the app no longer hangs when you click on a project
introduced in cd5f4e298446233e00b7044be81fabd77f81ee4a

api/services/utils/project.js
@@ -46,6 +46,7 @@ var getMetadata = function(proj, user, cb) {
       Like.findOne({ where: { userId: user.id, projectId: proj.id }}, function (err, like) {
         if (err) { return cb(err, proj); }
         if (like) { proj.like = true; }
+        return cb(err, proj);
       });
     });
   });