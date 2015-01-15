83b5198dc71798c59c749b6a4529b5c2fc8d7074
Dave Cole <david.cole@gsa.gov>
2015-01-15 16:54:42 -0500

    Comment out updatedUser notification for now

--- api/models/User.js
+++ api/models/User.js
@@ -45,23 +45,24 @@ module.exports = {
       type: 'INTEGER',
       defaultsTo: 0
     }
-  },
-
-  afterUpdate: function(model, done) {
-    Notification.create({
-      callerType: 'User',
-      callerId: model.id,
-      triggerGuid: require('node-uuid').v4(),
-      action: 'userUpdated',
-      createdDate: model.createdAt
-    }).exec(function (err, newNotification){
-      if (err) {
-        sails.log.debug(err);
-        done(null);
-        return false;
-      }
-      done(null);
-    });
   }
+
+  // Blocked by 18f/midas#498
+  // afterUpdate: function(model, done) {
+  //   Notification.create({
+  //     callerType: 'User',
+  //     callerId: model.id,
+  //     triggerGuid: require('node-uuid').v4(),
+  //     action: 'userUpdated',
+  //     createdDate: model.createdAt
+  //   }).exec(function (err, newNotification){
+  //     if (err) {
+  //       sails.log.debug(err);
+  //       done(null);
+  //       return false;
+  //     }
+  //     done(null);
+  //   });
+  // }
 
 };
