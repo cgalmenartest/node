dab8a02c109f46f8c902c5ff94a4cd79879e272d
Brandt Heisey <heiseybj@state.gov>
2015-04-08 15:04:36 -0400

    moved file logging to local.js

--- config/log.js
+++ config/log.js
@@ -1,5 +1,5 @@
 console.log('Loading... ', __filename);
-var winston = require('winston');
+//var winston = require('winston');
 /**
 * Logger configuration
 *
@@ -14,6 +14,19 @@ var winston = require('winston');
 Winston log levels are flipped from sails log levels, so using the log_level array makes it behave as expected in the sails documentation
 */
 
+/**************************************
+/**************************************
+ ***
+ ***      OVER RIDE INSTRUCTIONS
+ ***
+ **************************************
+/**************************************/
+
+/*
+to customize your logging settings add something like the following to the portion of your local.js file ABOVE where module.exports = { begins
+
+  var winston = require('winston');
+
 var log_levels = {
   silent: 6,
   error: 5,
@@ -26,14 +39,13 @@ var log_levels = {
 
 var transports = [
     new (winston.transports.File)(
-      /*This Transport controls what is written to the log file*/
       {
         level: 'verbose',
         name: 'written.log',
         silent: false,
         colorize: false,
         timestamp: true,
-        filename: "application.log",
+        filename: "application-test.log",
         maxsize: 500000,
         maxFiles: 5,
         json: false,
@@ -41,12 +53,9 @@ var transports = [
       }
     ),
     new (winston.transports.Console)(
-      /*This Transport is required to get console output, regardless of sails log level
-      *  output will be the lesser of sails log level and the transport level if they aren't equal
-      *  accordingly, transport log level shoul always be equal or lower than sails log level for readability's sake
-      */
+
       {
-        level: 'silly',
+        level: 'verbose',
         name: 'console.log',
         silent: false,
         colorize: true,
@@ -63,9 +72,58 @@ var logger = new (winston.Logger)({
   levels: log_levels
 });
 
-/* Don't forget to set the sails logging level to  a higher or equal level to what you want in the logs */
+ ************************************
+ ************************************
+ ************************************
+
+ AND THEN add a log section inside the module exports object
+    this level needs to be equal to or greater than the level you want to capture in the winston logs
+
+ log: {
+    level: 'verbose',
+    colors: false, // To get clean logs without prefixes or color codings
+    custom: logger
+  }
+*/
+
+var winston = require('winston');
+
+var log_levels = {
+  silent: 6,
+  error: 5,
+  warn: 4,
+  debug: 3,
+  info: 2,
+  verbose: 1,
+  silly: 0
+};
+
+var transports = [
+    new (winston.transports.Console)(
+      /*This Transport is required to get console output, regardless of sails log level
+      *  output will be the lesser of sails log level and the transport level if they aren't equal
+      *  accordingly, transport log level shoul always be equal or lower than sails log level for readability's sake
+      */
+      {
+        level: 'debug',
+        name: 'console.log',
+        silent: false,
+        colorize: true,
+        timestamp: true,
+        json: false,
+        handleExceptions: false
+      }
+    )
+  ];
+
+var logger = new (winston.Logger)({
+  exitOnError: false,
+  transports: transports,
+  levels: log_levels
+});
+
 module.exports.log = {
-  level: 'silly',
-  colors: false, // To get clean logs without prefixes or color codings
+  level: 'debug',
+  colors: false,
   custom: logger
 };
