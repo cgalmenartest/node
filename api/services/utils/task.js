99d3de019a2cf8ff4e073901276b2db28f18738d
Brendan Whitfield <brendanw@windworksdesign.com>
2015-06-15 14:28:35 -0400

    handling NULL query for task lookup

--- api/services/utils/task.js
+++ api/services/utils/task.js
@@ -15,6 +15,7 @@ var authorized = function (id, userId, user, cb) {
   }
   Task.findOneById(id).populate('tags').exec(function (err, task) {
     if (err) { return cb('Error finding task.', null); }
+    if (!task) { return cb('Task not found.', null); }
     task.isOwner = false;
     // otherwise, check that we have an owner
     if (userId && (userId == task.userId)) {
