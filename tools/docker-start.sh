e81c2bff8754717cc51a5e4377bd059edf58b697
Dave Cole <david.cole@gsa.gov>
2015-08-27 11:18:47 -0400

    Fix repo url

--- tools/docker-start.sh
+++ tools/docker-start.sh
@@ -6,7 +6,7 @@ if [ $BRANCH ]; then
   git config --global user.name "Example Name"
   git checkout -b $BRANCH master
   git checkout $BRANCH
-  git pull git://github.com/18f/federalist.git $BRANCH
+  git pull git://github.com/18f/midas.git $BRANCH
   if [ "$BRANCH" !=  "master" ]; then
     npm install --unsafe-perm
   fi
