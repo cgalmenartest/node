e3d607b0692d772f5e3bed54160da022edf02f40
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-08-31 20:57:28 -0400

    Display open task count in banner

--- assets/js/backbone/apps/home/views/home_dashboard_view.js
+++ assets/js/backbone/apps/home/views/home_dashboard_view.js
@@ -3,7 +3,8 @@ var Backbone = require('backbone');
 var async = require('async');
 var UIConfig = require('../../../config/ui.json');
 
-var ActivityCollection = require('../../../entities/activities/activity_collection');
+var ActivityCollection = require('../../../entities/activities/activities_collection');
+var TaskCollection = require('../../../entities/tasks/tasks_collection');
 
 var DashboardTemplate = require('../templates/home_dashboard_template.html');
 
@@ -18,14 +19,27 @@ var DashboardView = Backbone.View.extend({
   },
 
   render: function () {
-    var self = this,
-        activities = new ActivityCollection();
+    var self       = this,
+        activities = new ActivityCollection(),
+        tasks      = new TaskCollection();
 
     this.$el.addClass('home');
     this.listenTo(activities, 'activity:collection:fetch:success', function (e) {
       var html = self.template({ data: activities.toJSON() });
       self.$el.html(html);
       self.$el.i18n();
+    });
+
+    tasks.fetch({
+      success: function (collection) {
+        var open = collection.filter(function(t) {
+          return t.attributes.state !== 'completed';
+        });
+        $('#opportunity-count span').text(open.length);
+      },
+      error: function () {
+        $('#opportunity-count span').text('many');
+      }
     });
 
     return this;
