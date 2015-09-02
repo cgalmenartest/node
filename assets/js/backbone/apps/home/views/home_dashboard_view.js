8ecfd625cf571c2f3c56234e11147d5570be9d2d
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-09-02 12:32:18 -0400

    Break out dashboard into smaller templates

--- assets/js/backbone/apps/home/views/home_dashboard_view.js
+++ assets/js/backbone/apps/home/views/home_dashboard_view.js
@@ -3,15 +3,23 @@ var Backbone = require('backbone');
 var async = require('async');
 var UIConfig = require('../../../config/ui.json');
 
-var ActivityCollection = require('../../../entities/activities/activities_collection');
+var ActivityCollection = window.c = require('../../../entities/activities/activities_collection');
 var TaskCollection = require('../../../entities/tasks/tasks_collection');
 
 var DashboardTemplate = require('../templates/home_dashboard_template.html');
+var BadgesTemplate = require('../templates/home_badges_feed_template.html');
+var UsersTemplate = require('../templates/home_users_feed_template.html');
+var NetworkTemplate = require('../templates/home_network_stats_template.html');
+
+var templates = {
+  main: _.template(DashboardTemplate),
+  badges: _.template(BadgesTemplate),
+  users: _.template(UsersTemplate),
+  network: _.template(NetworkTemplate)
+};
 
 var DashboardView = Backbone.View.extend({
-
   el: "#container",
-  template: _.template(DashboardTemplate),
   initialize: function (options) {
     this.options = options;
 
@@ -19,15 +27,24 @@ var DashboardView = Backbone.View.extend({
   },
 
   render: function () {
-    var self       = this,
-        activities = new ActivityCollection(),
-        tasks      = new TaskCollection();
+    var self            = this,
+        badges          = new ActivityCollection({ type: 'badges' }),
+        users           = new ActivityCollection({ type: 'users' }),
+        tasks           = new TaskCollection();
 
+    this.$el.html(templates.main());
     this.$el.addClass('home');
-    this.listenTo(activities, 'activity:collection:fetch:success', function (e) {
-      var html = self.template({ data: activities.toJSON() });
-      self.$el.html(html);
-      self.$el.i18n();
+
+    this.listenTo(badges, 'activity:collection:fetch:success', function (e) {
+      var data = { badges: e.toJSON() },
+          badgesHtml = templates.badges(data);
+      self.setTarget('badges-feed', badgesHtml);
+    });
+
+    this.listenTo(users, 'activity:collection:fetch:success', function (e) {
+      var data = { users: e.toJSON() },
+          usersHtml = templates.users(data);
+      self.setTarget('users-feed', usersHtml);
     });
 
     tasks.fetch({
@@ -35,16 +52,30 @@ var DashboardView = Backbone.View.extend({
         var open = collection.filter(function(t) {
           return t.attributes.state !== 'completed';
         });
-        $('#opportunity-count span').text(open.length);
+        self.$('#opportunity-count span').text(open.length);
       },
       error: function () {
-        $('#opportunity-count span').text('many');
+        console.log('err with fetching task collection\n', err);
       }
     });
 
+    $.ajax('/api/activity/count', {
+      success: function (d) {
+        var html = templates.network({ count: d })
+        self.setTarget('network-stats', html);
+      },
+      error: function (err) {
+        console.log('err with /api/activity/count\n', err);
+      }
+    });
+
+    this.$el.i18n();
     return this;
   },
-
+  setTarget: function (target, inner) {
+    var s = '[data-target=' + target + ']';
+    $(s).html(inner);
+  },
   cleanup: function () {
     this.$el.removeClass('home');
     removeView(this);
