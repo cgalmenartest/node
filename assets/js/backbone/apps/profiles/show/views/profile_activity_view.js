1447ffaa8933d22f839f41e3cf182ec3f0a7e410
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-10-26 16:40:30 -0400

    Filter out invalid tasks for the activity view
    For some reason the API was return invalid task objects
    on the activity endpoint, which caused #1098. The API should
    not return `null` objects instead of tasks, but now the
    client should at least not crash if it gets a few invalid
    task objects from the API.

--- assets/js/backbone/apps/profiles/show/views/profile_activity_view.js
+++ assets/js/backbone/apps/profiles/show/views/profile_activity_view.js
@@ -18,9 +18,13 @@ var ProfileActivityView = Backbone.View.extend({
 
   render: function () {
     // sort initially by date, descending.
-    var results = this.options.data.sort(function (a, b) {
-      return new Date(b.createdAt) - new Date(a.createdAt);
-    });
+    var results = this.options.data
+        .filter(function(i) {
+          return i && i.createdAt;
+        })
+        .sort(function (a, b) {
+          return new Date(b.createdAt) - new Date(a.createdAt);
+        });
     var data = {
       ui: UIConfig,
       target: this.options.target,
