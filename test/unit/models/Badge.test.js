e7fb4502f0d73e322ecf67f9117f5ee163998aeb
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-11-04 21:06:02 -0500

    Test for task publishing badges
    Test for the award cases that are triggered by publishing a task

--- test/unit/models/Badge.test.js
+++ test/unit/models/Badge.test.js
@@ -3,22 +3,25 @@ var conf = require('../../api/sails/helpers/config');
 
 describe('BadgeModel', function() {
   var user;
-  var task;
+
+  before(function(done){
+    User.create(conf.defaultUser).exec(function createCB(err, newUser){
+      user = newUser;
+      done(err);
+    });
+  })
 
   describe('#awardForTaskCompletion()', function() {
+    var task;
+
     before(function(done) {
-      task = {id:1}
-      User.create(conf.defaultUser).exec(function createCB(err, new_user){
-        user = new_user;
-        console.log('Created user', user);
-        done(err);
-      });
+      task = { id:1 };
+      done();
     });
 
     describe('no tasks', function() {
       it('should not award a badge', function (done) {
         Badge.awardForTaskCompletion(task, user, function(err, badges) {
-          console.log('badges awarded', badges)
           assert.equal(badges.length, 0, 'badge should not be created');
           done();
         });
@@ -31,12 +34,112 @@ describe('BadgeModel', function() {
         user.save(function(err, user) {
           if (err) done(err);
           Badge.awardForTaskCompletion(task, user, function(err, badges) {
-            console.log('badges awarded', badges)
             assert.equal(badges.length, 1, 'badge should be created');
             done();
           });
         });
       });
     });
+
+    describe('2 tasks', function() {
+      it('should not award a new badge', function (done) {
+        user.completedTasks = 2;
+        user.save(function(err, user) {
+          if (err) done(err);
+          Badge.awardForTaskCompletion(task, user, function(err, badges) {
+            // this badge array contains NEWLY awarded badges
+            assert.equal(badges.length, 0, 'badge should not be created');
+            done();
+          });
+        });
+      });
+    });
+
   });
+
+  describe('#awardForTaskPublish()', function() {
+    var tasks,
+        tags = {
+          oneTime: {
+            name: 'One time',
+            type: 'task-time-required'
+          },
+          ongoing: {
+            name: 'Ongoing',
+            type: 'task-time-required'
+          }
+        };
+
+    describe('first one time task', function(){
+      it('should award a badge', function (done) {
+        var tasks = [
+          {
+            id: 1,
+            title: 'Fake one time task',
+            tags: [tags.oneTime]
+          }
+        ];
+        Badge.awardForTaskPublish(tasks, user.id, function(err, badges) {
+          // this badge array contains NEWLY awarded badges
+          assert.equal(badges.length, 1, 'badge should be created');
+          done();
+        });
+
+      });
+    });
+
+    describe('second one time task', function(){
+      it('should not award a badge', function (done) {
+        var tasks = [
+          {
+            id: 2,
+            title: 'Second fake one time task',
+            tags: [tags.oneTime]
+          }
+        ];
+        Badge.awardForTaskPublish(tasks, user.id, function(err, badges) {
+          assert.isDefined(err);
+          done();
+        });
+
+      });
+    });
+
+    describe('first ongoing task', function(){
+      it('should award a badge', function (done) {
+        var tasks = [
+          {
+            id: 1,
+            title: 'Fake ongoing task',
+            tags: [tags.ongoing]
+          }
+        ];
+        Badge.awardForTaskPublish(tasks, user.id, function(err, badges) {
+          // this badge array contains NEWLY awarded badges
+          assert.equal(badges.length, 1, 'badge should be created');
+          done();
+        });
+
+      });
+    });
+
+    describe('second ongoing task', function(){
+      it('should not award a badge', function (done) {
+        var tasks = [
+          {
+            id: 2,
+            title: 'Second fake one time task',
+            tags: [tags.ongoing]
+          }
+        ];
+        Badge.awardForTaskPublish(tasks, user.id, function(err, badges) {
+          assert.isDefined(err);
+          done();
+        });
+
+      });
+    });
+
+  });
+
 });
