b170c4051374a23ec0a41932a3fd560860a3fd6b
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2015-12-30 16:38:04 -0500

    Update Admin Controller and Templates;
    This patch updates the Admin controllers, templates, and views on both
    the client and server to support the Save as Draft feature.

--- assets/js/backbone/apps/admin/views/admin_task_view.js
+++ assets/js/backbone/apps/admin/views/admin_task_view.js
@@ -3,18 +3,22 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var utils = require('../../../mixins/utilities');
 var AdminTaskTemplate = require('../templates/admin_task_template.html');
+var TaskModel = require( '../../../entities/tasks/task_model' );
 
 
 var AdminTaskView = Backbone.View.extend({
 
   events: {
-    'click .delete-task': 'deleteTask'
+
+    'click .delete-task'  : 'deleteTask',
+    'click .js-task-open' : 'openTask',
+
   },
 
   initialize: function (options) {
     this.options = options;
     this.data = {
-      page: 1
+      page: 1,
     };
   },
 
@@ -34,6 +38,53 @@ var AdminTaskView = Backbone.View.extend({
 
     Backbone.history.navigate('/admin/tasks');
     return this;
+  },
+
+
+  /*
+   * Open a "submitted" task from the admin task view.
+   * @param { jQuery Event } event
+   */
+  openTask: function ( event ) {
+
+    event.preventDefault();
+
+    var view = this;
+    var id = $( event.currentTarget ).data( 'task-id' );
+    var title = $( event.currentTarget ).data( 'task-title' );
+
+    var task = new TaskModel( { id: id } );
+
+    task.fetch( {
+
+      success: function ( model, response, options ) {
+
+        var userConfirmed = window.confirm( 'Are you sure you want to open "' + model.attributes.title + '"?' );
+
+        if ( userConfirmed ) {
+
+          model.save( {
+
+            state: 'open',
+
+          }, {
+
+            success: function ( model, response, options ) {
+
+              view.render();
+
+            },
+
+          } );
+
+        }
+
+      },
+
+      error: function ( model, response, options ) {},
+
+    } );
+
   },
 
   deleteTask: function(e) {
