51b02aedc3e7ec5b097cef7713e67b59b12be0be
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2015-12-30 16:36:59 -0500

    Lint notification JS

--- api/notifications/task.update.opened/notification.js
+++ api/notifications/task.update.opened/notification.js
@@ -10,15 +10,15 @@ module.exports = {
   * @param {function} callback called with err, data
   * data.globals defaults to sails.config
   */
-  data: function(model, done) {
+  data: function (model, done) {
     var data = {
-          task: model,
-          user: {}
-        };
-    User.findOne({ id: model.userId }).exec(function(err, user) {
+      task: model,
+      user: {},
+    };
+    User.findOne({ id: model.userId }).exec(function (err, user) {
       if (err) return done(err);
       data.user = user;
       done(null, data);
     });
-  }
+  },
 };
