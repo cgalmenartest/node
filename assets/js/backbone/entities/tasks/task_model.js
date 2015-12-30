4077dd52ef2937ee89c152d07d5530c6c30fbdfa
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2015-12-30 11:22:15 -0500

    Lint TaskModel

--- assets/js/backbone/entities/tasks/task_model.js
+++ assets/js/backbone/entities/tasks/task_model.js
@@ -19,27 +19,23 @@ var TaskModel = Backbone.Model.extend({
 
   initialize: function () {
 
-    this.listenTo( this, 'task:draft', function ( data ) {
-      this.save( data );
-    } );
-
-    this.listenTo(this, "task:save", function (data) {
+    this.listenTo(this, 'task:save', function (data) {
       this.save(data);
     });
 
-    this.listenTo(this, "task:model:fetch", function (data) {
+    this.listenTo(this, 'task:model:fetch', function (data) {
       this.remoteGet(data);
     });
 
-    this.listenTo(this, "task:update", function (data) {
+    this.listenTo(this, 'task:update', function (data) {
       this.update(data);
     });
 
-    this.listenTo(this, "task:update:state", function (state) {
+    this.listenTo(this, 'task:update:state', function (state) {
       this.updateState(state);
     });
 
-    this.listenTo(this, "task:update:orphan", function (data) {
+    this.listenTo(this, 'task:update:orphan', function (data) {
       this.orphan(data);
     });
   },
@@ -48,8 +44,8 @@ var TaskModel = Backbone.Model.extend({
     var self = this;
     this.save(data, {
       success: function (data) {
-        self.trigger("task:update:success", data);
-      }
+        self.trigger('task:update:success', data);
+      },
     });
   },
 
@@ -93,4 +89,4 @@ var TaskModel = Backbone.Model.extend({
 
 });
 
-  module.exports = TaskModel;
+module.exports = TaskModel;
