5e632a05e82d7f31baeaf88c67dfd2b1eaf74408
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2015-12-31 13:42:16 -0500

    Lint TaskShowController

--- assets/js/backbone/apps/tasks/show/controllers/task_show_controller.js
+++ assets/js/backbone/apps/tasks/show/controllers/task_show_controller.js
@@ -25,22 +25,22 @@ var popovers = new Popovers();
 
 var TaskShowController = BaseView.extend({
 
-  el: "#container",
+  el: '#container',
 
   events: {
-    'change .validate'                : 'v',
-    'keyup .validate'                 : 'v',
-    'click #task-edit'                : 'edit',
-    'click #task-view'                : 'view',
-    'click #volunteer'                : 'volunteer',
-    'click #volunteered'              : 'volunteered',
-    "click #task-close"               : "stateChange",
-    "click #task-reopen"              : "stateReopen",
-    "click #task-copy"                : "copy",
-    "click .link-backbone"            : linkBackbone,
-    "click .delete-volunteer"         : 'removeVolunteer',
-    "mouseenter .project-people-show-div"  : popovers.popoverPeopleOn,
-    "click .project-people-show-div"       : popovers.popoverClick
+    'change .validate'                    : 'v',
+    'keyup .validate'                     : 'v',
+    'click #task-edit'                    : 'edit',
+    'click #task-view'                    : 'view',
+    'click #volunteer'                    : 'volunteer',
+    'click #volunteered'                  : 'volunteered',
+    'click #task-close'                   : 'stateChange',
+    'click #task-reopen'                  : 'stateReopen',
+    'click #task-copy'                    : 'copy',
+    'click .link-backbone'                : linkBackbone,
+    'click .delete-volunteer'             : 'removeVolunteer',
+    'mouseenter .project-people-show-div' : popovers.popoverPeopleOn,
+    'click .project-people-show-div'      : popovers.popoverClick,
   },
 
   initialize: function (options) {
@@ -452,19 +452,33 @@ var TaskShowController = BaseView.extend({
 
     if (this.modalAlert) { this.modalAlert.cleanup(); }
     if (this.modalComponent) { this.modalComponent.cleanup(); }
+
     var states = UIConfig.states;
-    if (draftAdminOnly && !window.cache.currentUser.isAdmin) {
-      states = _(states).reject(function(state) {
+
+    if ( draftAdminOnly && !window.cache.currentUser.isAdmin ) {
+
+      states = _( states ).reject( function ( state ) {
         return state.value === 'draft';
-      });
+      } );
+
     }
 
-    var modalContent = _.template(ChangeStateTemplate)({model:self.model,states: states});
-    this.modalComponent = new ModalComponent({
-      el: "#modal-close",
-      id: "check-close",
-      modalTitle: "Change "+i18n.t("Task")+" State"
-    }).render();
+    var modalData = {
+
+      model: self.model,
+      states: states,
+
+    };
+
+    var modalContent = _.template( ChangeStateTemplate )( modalData );
+
+    this.modalComponent = new ModalComponent( {
+
+      el: '#modal-close',
+      id: 'check-close',
+      modalTitle: 'Change '+i18n.t( 'Task' ) + ' State',
+
+    } ).render();
 
     this.modalAlert = new ModalAlert({
       el: "#check-close .modal-template",
