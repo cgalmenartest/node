a199790bc05601894efec2da561943cc2505605d
Sarah Allen <sarah.allen@gsa.gov>
2016-04-05 06:08:00 -0700

    detailed model test with code coverage
    maybe not factored quite right
    perhaps #toJSON should be tested in controller where it is used
    also duplicate record error doesn’t report error when it should
    even though sails-memory waterline tests pass for that case

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,37 +1,99 @@
-var assert = require('chai').assert;
+var chai = require('chai');
+var assert = chai.assert;
+chai.use(require('chai-datetime'));
 
 describe('UserModel', function() {
-  var userAttrs = {
+  var createAttrs = {
+    'name': 'Maria Sanchez',
+    'username': 'MARIA@openopps.org',
+    'password': 'TestTest123#',
+    'bio': 'JavaScript expert with an interest in public policy.'
+  };
+  var expectedAttrs = {
+    'id': 1,
     'name': 'Maria Sanchez',
     'username': 'maria@openopps.org',
-    'password': 'TestTest123#'
+    'bio': 'JavaScript expert with an interest in public policy.'
+    // also expect createdAt and updatedAt
   };
 
-  describe('#create', function() {
-    describe('new user', function() {
-      var newUser = {};
-      before(function(done) {
-        User.create(userAttrs)
-        .then(function(result) {
-            newUser = result;
-            done();
-        })
-        .catch(done);
-      });
+  describe('new user', function() {
+    var newUser = {};
+    before(function(done) {
+      User.create(createAttrs)
+      .then(function(result) {
+          newUser = result;
+          done();
+      })
+      .catch(done);
+    });
+    describe('#create', function() {
       it('should have username', function (done) {
-          assert.equal(newUser.username, userAttrs.username);
+          assert.equal(newUser.username, expectedAttrs.username);
           done();
       });
       it('should have name', function (done) {
-          assert.equal(newUser.name, userAttrs.name);
+          assert.equal(newUser.name, expectedAttrs.name);
           done();
       });
       it('should not have password', function (done) {
+          // this is really testing that random attributes are not saved
           assert.notProperty(newUser, 'password');
           done();
       });
-
+      it('should have zero passports', function (done) {
+          assert.equal(newUser.passports.length, 0);
+          done();
+      });
+    });
+    describe('#toJSON', function() {
+      it('has created date', function (done) {
+        assert.instanceOf(newUser.createdAt, Date);
+        done();
+      });
+      it('has updated same as created', function (done) {
+        assert.equalDate(newUser.updatedAt, newUser.createdAt);
+        done();
+      });
+      it('has expected properties', function (done) {
+          newUserAttrs = newUser.toJSON()
+          delete newUserAttrs.createdAt // tested above
+          delete newUserAttrs.updatedAt // tested above
+          assert.deepEqual(newUserAttrs, expectedAttrs);
+          done();
+      });
     });
   });
 
+  describe('errors:', function() {
+    var newUser = {};
+    before(function(done) {
+      User.create(createAttrs)
+      .then(function(result) {
+          newUser = result;
+          //done();
+
+          User.find({username: createAttrs.username}, function(err, records) {
+            assert(!err);
+            assert.equal(records.length, 1);
+            done();
+          });
+      })
+      .catch(done);
+    });
+    //WLValidationError
+    describe('#create', function() {
+      it('should fail with duplicate username (email)', function (done) {
+        User.create({username: createAttrs.username}, function(err, records) {
+          //assert.isNotNull(err, 'expect an error from User.create');
+          //assert.equal(err.code, 'E_VALIDATION');
+          User.find({username: createAttrs.username}, function(err, records) {
+            assert(!err);
+            assert.equal(records.length, 1);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
