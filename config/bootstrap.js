c733afebbf44afaca96e25d1cd2bbdc9d902a0bb
Sarah Allen <sarah.allen@gsa.gov>
2016-04-07 20:09:23 -0700

    tag autocomplete tests & implementation
    remove wikipedia autocomplete
    load services from nested directories
    remove TagEntityController — not sure it is used by client

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -8,12 +8,30 @@
  * For more information on bootstrapping your app, check out:
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.bootstrap.html
  */
+var buildDictionary = require('sails-build-dictionary');
 
 module.exports.bootstrap = function(cb) {
 
+  // TODO: ennable for uploads
+  // If upload directory is missing, create it
+  // var config = sails.config.fileStore || {},
+  //     dir = config.local.dirname || '/assets/uploads',
+  //     path = p.join(sails.config.appPath, dir);
+  // if (!fs.existsSync(path)) fs.mkdirSync(path);
+
   sails.services.passport.loadStrategies();
 
-  // It's very important to trigger this callback method when you are finished
-  // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  cb();
+  // loads services in nested folders
+  // for example: autocomplete sources
+  buildDictionary.optional({
+        dirname     : sails.config.paths.services,
+        filter      : /(.+)\.(js|coffee|litcoffee)$/,
+        depth     : 2,
+        caseSensitive : true
+      }, function (err, modules) {
+           sails.services = modules;
+           // It's very important to trigger this callback method when you are finished
+           // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
+           cb();
+      });
 };
