f2ed0748e9ac73955ea0ee5bbca7a8535d7d58ad
Brendan Sudol <brendan.sudol@gsa.gov>
2016-04-11 10:56:59 -0400

    remove less style compilation step

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
             files: [{
                 expand: true,
                 cwd: 'assets/styles/',
-                src: ['importer2.scss'],
+                src: ['importer.scss'],
                 dest: '.tmp/public/styles/',
                 ext: '.css'
             }]
