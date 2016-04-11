fb4dccef0e06aab84c9eced2a87b208b1da05871
Brendan Sudol <brendan.sudol@gsa.gov>
2016-04-11 12:55:43 -0400

    migration of basic app stylez

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -1,16 +1,58 @@
+var _ = require('underscore');
+
+
 module.exports = function(grunt) {
 
-    grunt.config.set('sass', {
-        dev: {
-            files: [{
-                expand: true,
-                cwd: 'assets/styles/',
-                src: ['main.scss'],
-                dest: '.tmp/public/styles/',
-                ext: '.css'
-            }]
-        }
-    });
+  grunt.config.set('sass', {
+    dev: {
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
+      // sass: {
+
+      //   options: {
+
+      //     style: 'expanded',
+      //     sourcemaps: true,
+
+      //     includePaths: _.flatten([
+      //       require('bourbon').includePaths,
+      //       'assets/styles',
+      //       'assets/styles/vendor',
+      //       'node_modules',
+      //     ]),
+
+      //   }, 
+      //   dist: {
+      //       files: {
+      //           'assets/styles/FOO.css': 'assets/styles/main.scss'
+      //       }
+      //   },
+      //   app: {
+      //     options: {
+      //       style: 'compressed',
+      //     },
+      //     files: {
+      //       'assets/build/css/midas.css': [
+      //         'assets/styles/main.scss',
+      //       ],
+      //     },
+
+      //   },
+      // },
+
+      files: [{
+        expand: true,
+        cwd: 'assets/styles/',
+        loadPath: [
+          'assets/styles',
+          'assets/styles/vendor',
+          'node_modules',
+        ],
+        src: ['main.scss'],
+        dest: '.tmp/public/styles/',
+        ext: '.css',
+      }]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-sass');
 };
