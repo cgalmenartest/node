65491837e09fff1d9eeb41521a716b248c678189
Sarah Allen <sarah.allen@gsa.gov>
2016-04-20 14:43:09 -0700

    rename requireUserAuth policy => requireAuth
    shorter name is easier, “User” is implied

--- api/policies/requireAuth.js
+++ api/policies/requireAuth.js
@@ -2,7 +2,7 @@
 * Require the user to be logged in.
 */
 
-module.exports = function requireUserAuth(req, res, next) {
+module.exports = function requireAuth(req, res, next) {
   sails.log.verbose('requireUserAuth policy, req.user: ', req.user);
   if (!req.user) {
     return res.forbidden('Not authorized');
