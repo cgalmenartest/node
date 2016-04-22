3a49eaf02ee9145950c0df4a3bfd76b302389112
Claire Sarsam <csarsam@gmail.com>
2016-04-22 11:03:21 -0700

    fix user test

--- test/fixtures/user.js
+++ test/fixtures/user.js
@@ -5,10 +5,14 @@ module.exports = {
     'password': 'TestTest123#'
   },
   'allAttrs': {
-    'name': 'Maria Sanchez',
-    'username': 'maria@openopps.org',
-    'password': 'TestTest123#',
-    'completedTasks': 0,
-    'bio': 'JavaScript expert with an interest in public policy.'
-  },
+    name: 'Maria Sanchez',
+    username: 'maria@openopps.org',
+    password: 'TestTest123#',
+    disabled: false,
+    isAdmin: false,
+    isAgencyAdmin: false,
+    passwordAttempts: 0,
+    completedTasks: 0,
+    bio: 'JavaScript expert with an interest in public policy.'
+  }
 }
