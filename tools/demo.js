ff7308a8755a7fe84cb17077cef2cd7f0ac77fe3
Sarah Allen <sarah.allen@gsa.gov>
2016-04-28 03:07:52 -0700

    use promises to make code nicer

--- tools/demo.js
+++ tools/demo.js
@@ -1,40 +1,37 @@
 var async = require("async");
 var fs = require("fs");
+var Promise = require('bluebird');
+
 var Sails = require('sails').constructor;
 var sailsApp = new Sails();
 
 var dbTools = require('./lib/dbTools');
 var sailsModel = require('./lib/modelTools');
 var dataDir = __dirname + '/demo-data'
+var sailsLift = Promise.promisify(sailsApp.lift, {context: sailsApp});
 
 dbTools.checkTableSetup('midas_user')
 .then(function() {
-  sailsApp.lift(
-    {
+  return sailsLift({
       log: {
         level: 'error'
       }
-    },
-    function (err, liftedApp) {
-      if (err) {
-        return console.log("Error occurred lifting Sails app: ", err);
-      }
-      console.log('config connections:', liftedApp.config.connections)
-      console.log('config models.connection:', liftedApp.config.models.connection)
-
-      sailsModel.importFromFile(sails.models['user'], dataDir + '/users.csv')
-      .then(function(users) {
-        console.log('new users created: ', users);
-        return sailsModel.importFromFile(sails.models['task'], dataDir + '/tasks.csv')
-      })
-      .then(function(tasks) {
-        console.log('new tasks created: ', tasks);
-        console.log("Sails app lifted successfully in port", sailsApp.config.port);
-        // Note that liftedApp === sailsApp
-      })
-      .catch(function(err) {
-        console.log('err: ', err)
-      });
-    }
-  )
+    });
+})
+.then(function(liftedApp) {
+  console.log('config connections:', liftedApp.config.connections);
+  console.log('config models.connection:', liftedApp.config.models.connection);
+  return sailsModel.importFromFile(sails.models['user'], dataDir + '/users.csv');
+})
+.then(function(users) {
+  console.log('new users created: ', users);
+  return sailsModel.importFromFile(sails.models['task'], dataDir + '/tasks.csv')
+})
+.then(function(tasks) {
+  console.log('new tasks created: ', tasks);
+  console.log("Sails app lifted successfully: http://localhost:"+sailsApp.config.port);
+  // Note that liftedApp === sailsApp
+})
+.catch(function(err) {
+  console.log('err: ', err)
 });
