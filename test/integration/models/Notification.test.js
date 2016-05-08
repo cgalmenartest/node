cfeba31ef2f0a7e304c667de9006cb50dbd842cd
Sarah Allen <sarah.allen@gsa.gov>
2016-05-08 10:38:33 -0700

    fix notification template errors (userId => owner)
    with tests!

--- test/integration/models/Notification.test.js
+++ test/integration/models/Notification.test.js
@@ -3,7 +3,7 @@ var sinon = require('sinon');
 var taskFixtures = require('../../fixtures/task');
 var userFixtures = require('../../fixtures/user');
 
-describe('NotificationModel', function() {
+describe('Notification model', function() {
 
   beforeEach(function(done){
       done();
@@ -57,43 +57,78 @@ describe('NotificationModel', function() {
 
   describe('#trigger', function() {
     var user, task;
+    var email;
 
     function checkTriggerEmail(data, done) {
-      var email;
-      Notification._transport.sendMail = function mockSend(options, cb) {
-        email = options;
-        cb(null);
-      };
       Notification.trigger(data, function(err, info) {
         assert.isNull(err);
         done(err, email);
       });
     }
 
-    before(function(done) {
+    beforeEach(function(done) {
+      Notification._transport.sendMail = function mockSend(options, cb) {
+        email = options;
+        cb(null);
+      };
+
       User.create(userFixtures.minAttrs)
       .then(function(newUser) {
         user = newUser;
-        return Task.create(taskFixtures.draft);
-      })
-      .then(function(newTask) {
-        task = newTask
-        sails.config.emailProtocol = 'SMTP';
         done();
       }).catch(done);
+    })
+    before(function() {
+      sails.config.emailProtocol = 'SMTP';
     })
     after(function() {
       sails.config.emailProtocol = '';
     })
 
+
     it('user.create.welcome', function (done) {
-      data = {action: 'user.create.welcome', model:user}
+      data = {action: 'user.create.welcome', model:user};
       checkTriggerEmail(data, function(err, email) {
         assert.equal(email.to, user.username);
         assert.equal(email.subject, "Welcome to "+sails.config.systemName);
         done();
       })
     });
+    describe('for draft task', function() {
+      beforeEach(function(done) {
+        Task.create(taskFixtures.draft)
+        .then(function(newTask) {
+          task = newTask;
+          done();
+        });
+      });
+      it('task.create.draft', function (done) {
+        data = {action: 'task.create.draft', model:task};
+        checkTriggerEmail(data, function(err, email) {
+          assert.equal(email.to, user.username);
+          assert.equal(email.subject, "New Open Opportunity Draft Created");
+          done();
+        })
+      });
+    });
+    describe('for submitted task', function() {
+      beforeEach(function(done) {
+        Task.create(taskFixtures.oneTimeSubmitted)
+        .then(function(newTask) {
+          task = newTask;
+          done();
+        });
+      });
+      it('task.create.thanks', function (done) {
+        data = {action: 'task.create.thanks', model:task}
+        checkTriggerEmail(data, function(err, email) {
+          assert.equal(email.to, user.username);
+          assert.equal(email.subject, "New Opportunity Submission");
+          done();
+        })
+      });
+    });
+
   });
 
 });
