5568402965e3320fab37bc3ada7823d9303c30b0
Sarah Allen <sarah@ultrasaurus.com>
2016-05-15 07:04:17 -0700

    changes required to make the theme work (#1322)
    also added analytics from 0.9.5

---
+++ config/settings/analytics.js
@@ -0,0 +1,19 @@
+console.log('Loading... ', __filename);
+
+// web analytics configuration
+module.exports = {
+  //ANALYTICS SETTINGS
+  google: {
+    enabled: false,
+    key: ''
+  },
+  piwik: {
+    enabled: false,
+    host: '',
+    key: ''
+  },
+  dap: {
+    enabled: false,
+    source: ''
+  }
+};
