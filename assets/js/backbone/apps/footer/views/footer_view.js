7d595070b525d9cd0f1beb528d2f2038c2ae7944
Sarah Allen <sarah.allen@gsa.gov>
2016-05-15 09:44:33 -0700

    milestone progress in footer link for pre-release
    version -> 0.10.1-beta
    for interim builds (typically deployed on openopps-test)
    now it is easy to see the changes queue up for next release
    maybe we should only tag release when it goes to master
    then we’d have more pre-release testing
    and fewer false starts

--- assets/js/backbone/apps/footer/views/footer_view.js
+++ assets/js/backbone/apps/footer/views/footer_view.js
@@ -8,7 +8,18 @@ var FooterTemplate = fs.readFileSync(
   __dirname + '/../templates/footer_template.html'
 ).toString();
 
-
+function versionLink(version) {
+  var link;
+  var parts = version.split('-');
+  if (parts.length === 1) {
+    link = "https://github.com/18F/openopps-platform/releases/tag/v" + parts[0];
+  } else {
+    // this is a pre-release version, like 0.10.1-beta
+    // since we won't have tagged a release yet, show the issues for this milestone
+    link = "https://github.com/18F/openopps-platform/issues?q=milestone%3A" + parts[0] + "+is%3Aclosed";
+  }
+  return link;
+}
 var FooterView = Backbone.View.extend({
   events: {},
 
@@ -16,6 +27,7 @@ var FooterView = Backbone.View.extend({
     var self = this;
     var data = {
       version: version,
+      versionLink: versionLink(version),
       login: Login
     };
     var compiledTemplate = _.template(FooterTemplate)(data);
