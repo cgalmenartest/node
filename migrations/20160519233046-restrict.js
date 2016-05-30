5f56555c2ee386b6e7c9d620e4c65330487e0c13
Sarah Allen <sarah@ultrasaurus.com>
2016-05-29 23:03:55 -0700

    migration for task, add ‘restrict’ column

---
+++ migrations/20160519233046-restrict.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function(db, callback) {
+  db.addColumn('task', 'restrict', {
+        type: 'string'
+      }, callback);
+};
+
+exports.down = function(db, callback) {
+  db.removeColumn('task', 'restrict', callback);
+};
