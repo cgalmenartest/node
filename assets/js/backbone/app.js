9ba98fb48eab2d4fbe2da58cb6ff55adf1e14b81
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-07-24 23:51:56 -0400

    Add CSRF protection;
    Addresses ZAP issue:
    
    > Web Browser XSS Protection Not Enabled

--- assets/js/backbone/app.js
+++ assets/js/backbone/app.js
@@ -19,11 +19,9 @@ Backbone.$ = $;
 // Set CSRF header
 $.ajaxPrefilter(function(options, originalOptions, jqXHR) {
   var token;
-  if (!options.crossDomain) {
-    token = $('meta[name="csrf-token"]').attr('content');
-    if (token) {
-      return jqXHR.setRequestHeader('X-CSRF-Token', token);
-    }
+  token = $('meta[name="csrf-token"]').attr('content');
+  if (token) {
+    return jqXHR.setRequestHeader('X-CSRF-Token', token);
   }
 });
 
