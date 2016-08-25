2f24ca59f3a39160d8af30c3e91e806c68911f3b
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2016-08-25 14:23:43 -0400

    Patch is so that owners can delete participants

--- api/policies/ownerOrAdmin.js
+++ api/policies/ownerOrAdmin.js
@@ -4,7 +4,9 @@
 
 module.exports = function ownerOrAdmin (req, res, next) {
   // check if owner of the task
-  if (req.isOwner || req.user.isAdmin) {
+  if (req.isOwner ||
+      ( req.body.taskId && req.body.taskId.isOwner ) ||
+      req.user.isAdmin) {
     return next();
   }
   // Otherwise not allowed.
